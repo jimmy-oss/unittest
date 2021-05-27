265722c51b0fdac554894d7d965b57202b41d4d0
[jimmy-oss] <jimmynjonge51@gmail.com>
2021-05-28 00:40:42 +0300

    contact_lists

--- contact.py
+++ contact.py
@@ -31,4 +31,25 @@ class Contact():
          for contact in cls.contact_list:
             if contact.phone_number == number:
                 return contact  
-        
+     
+    @classmethod
+    def contact_exist(cls,number):
+        '''
+        Method that checks if a contact exists from the contact list.
+        Args:
+            number: Phone number to search if it exists
+        Returns :
+            Boolean: True or false depending if the contact exists
+        '''
+        for contact in cls.contact_list:
+            if contact.phone_number == number:
+                    return True
+
+        return False
+    @classmethod
+    def display_contacts(cls):
+        '''
+        method that returns the contact list
+        '''
+        return cls.contact_list
+ 
--- contact_test.py
+++ contact_test.py
@@ -1,4 +1,5 @@
 import unittest # Importing the unittest module
+import pyperclip # Importing the pyperclip module
 from contact import Contact
 
 class TestContact(unittest.TestCase):
@@ -76,8 +77,42 @@ class TestContact(unittest.TestCase):
 
         found_contact = Contact.find_by_number("0711223344")
 
-        self.assertEqual(found_contact.email,test_contact.email)     
-            
+        self.assertEqual(found_contact.email,test_contact.email)  
+           
+   def test_contact_exists(self):
+        '''
+        test to check if we can return a Boolean  if we cannot find the contact.
+        '''
+
+        self.new_contact.save_contact()
+        test_contact = Contact("Test","user","0711223344","test@user.com") # new contact
+        test_contact.save_contact()
+
+        contact_exists = Contact.contact_exist("0711223344")
+
+        self.assertTrue(contact_exists)    
+          
+   def test_display_all_contacts(self):
+        '''
+        method that returns a list of all contacts saved
+        '''
+
+        self.assertEqual(Contact.display_contacts(),Contact.contact_list)
+        
+   def test_copy_email(self):
+        '''
+        Test to confirm that we are copying the email address from a found contact
+        '''
+
+        self.new_contact.save_contact()
+        Contact.copy_email("0712345678")
+
+        self.assertEqual(self.new_contact.email,pyperclip.paste())
+   @classmethod
+   def copy_email(cls,number):
+        contact_found = Contact.find_by_number(number)
+        pyperclip.copy(contact_found.email)
+     
 
 if __name__ == '__main__':
     unittest.main()
