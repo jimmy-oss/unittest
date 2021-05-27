5a097a6746ea470f7c380aee41cb94be04132a81
[jimmy-oss] <jimmynjonge51@gmail.com>
2021-05-27 13:00:15 +0300

    test1

---
+++ contact_test.py
@@ -0,0 +1,25 @@
+import unittest # Importing the unittest module
+from contact import Contact
+
+class TestContact(unittest.TestCase):
+   def setUp(self):
+        '''
+        Set up method to run before each test cases.
+        '''
+        self.new_contact = Contact("James","Muriuki","0712345678","james@ms.com") # create contact object
+
+   def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
+
+        self.assertEqual(self.new_contact.first_name,"James")
+        self.assertEqual(self.new_contact.last_name,"Muriuki")
+        self.assertEqual(self.new_contact.phone_number,"0712345678")
+        self.assertEqual(self.new_contact.email,"james@ms.com")
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+ 
