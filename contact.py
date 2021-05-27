615ccd849bc915d4dc95522fb7beb3560b61691d
[jimmy-oss] <jimmynjonge51@gmail.com>
2021-05-27 22:50:47 +0300

    unittests

--- contact.py
+++ contact.py
@@ -4,3 +4,31 @@ class Contact():
         self.last_name = last_name
         self.phone_number = number
         self.email = email
+
+    contact_list = [] # Empty contact list
+ # Init method up here
+    def save_contact(self):
+
+        '''
+        save_contact method saves contact objects into contact_list
+        '''
+
+        Contact.contact_list.append(self)
+    def delete_contact(self):
+
+        '''
+        delete_contact method deletes a saved contact from the contact_list
+        '''
+
+        Contact.contact_list.remove(self)
+
+    @classmethod
+
+    def find_by_number(cls,number):
+       
+         
+
+         for contact in cls.contact_list:
+            if contact.phone_number == number:
+                return contact  
+        
--- contact_test.py
+++ contact_test.py
@@ -17,9 +17,72 @@ class TestContact(unittest.TestCase):
         self.assertEqual(self.new_contact.last_name,"Muriuki")
         self.assertEqual(self.new_contact.phone_number,"0712345678")
         self.assertEqual(self.new_contact.email,"james@ms.com")
+    
+   def test_save_contact(self):
+        '''
+        test_save_contact test case to test if the contact object is saved into
+         the contact list
+        '''
+        self.new_contact.save_contact() # saving the new contact
+        self.assertEqual(len(Contact.contact_list),1)
+        
+   def test_save_multiple_contact(self):
+            '''
+            test_save_multiple_contact to check if we can save multiple contact
+            objects to our contact_list
+            '''
+            self.new_contact.save_contact()
+            test_contact = Contact("Test","user","0712345678","test@user.com") # new contact
+            test_contact.save_contact()
+            self.assertEqual(len(Contact.contact_list),2)
 
+       # setup and class creation up here
+   def tearDown(self):
+            '''
+            tearDown method that does clean up after each test case has run.
+            '''
+            Contact.contact_list = []
+
+# other test cases here
+   def test_save_multiple_contact(self):
+            '''
+            test_save_multiple_contact to check if we can save multiple contact
+            objects to our contact_list
+            '''
+            self.new_contact.save_contact()
+            test_contact = Contact("Test","user","0712345678","test@user.com") # new contact
+            test_contact.save_contact()
+            self.assertEqual(len(Contact.contact_list),2)
+            # More tests above
+   def test_delete_contact(self):
+            '''
+            test_delete_contact to test if we can remove a contact from our contact list
+            '''
+            self.new_contact.save_contact()
+            test_contact = Contact("Test","user","0712345678","test@user.com") # new contact
+            test_contact.save_contact()
+
+            self.new_contact.delete_contact()# Deleting a contact object
+            self.assertEqual(len(Contact.contact_list),1)
+
+   def test_find_contact_by_number(self):
+        '''
+        test to check if we can find a contact by phone number and display information
+        '''
+
+        self.new_contact.save_contact()
+        test_contact = Contact("Test","user","0711223344","test@user.com") # new contact
+        test_contact.save_contact()
+
+        found_contact = Contact.find_by_number("0711223344")
+
+        self.assertEqual(found_contact.email,test_contact.email)     
+            
 
 if __name__ == '__main__':
     unittest.main()
+ 
+
+ 
 
  
